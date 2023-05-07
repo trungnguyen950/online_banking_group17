1553926ac88c4e816ba721c10b5410f45e0595d3
trungnguyen950 <120566959+trungnguyen950@users.noreply.github.com>
2023-05-07 16:34:02 +0700

    first init

---
+++ src/test/java/edu/hanu/online_banking_group17/OnlineBankingGroup17ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hanu.online_banking_group17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingGroup17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
